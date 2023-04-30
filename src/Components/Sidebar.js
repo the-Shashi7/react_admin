8266db22980a8baef407fdd2e7031d753cc0d9db
the-Shashi7 <shashi.ic19@nsut.ac.in>
2023-04-30 19:56:07 +0530

    change in side color

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -26,42 +26,42 @@ export default function Sidebar() {
         <div className="main-elements">
           <img className="icons" src={dashboard} alt="mains"></img>
           <div className="main-name-icons">
-            <h5 style={{color:'#898989'}} >Dashboard</h5>
+            <h5 className='main-name' >Dashboard</h5>
             <h2 style={{color:'#898989'}}  className="main-greater-icons" >‹</h2>
           </div>
         </div>
         <div className="main-elements">
           <img className="icons" src={discover} alt="mains"></img>
           <div className="main-name-icons">
-            <h5 style={{color:'#898989'}} >Discover</h5>
+            <h5 className='main-name'>Discover</h5>
             <h2 style={{color:'#898989'}}  className="main-greater-icons" >‹</h2>
           </div>
         </div>
         <div className="main-elements">
           <img className="icons" src={users} alt="mains"></img>
           <div className="main-name-icons">
-            <h5 style={{color:'#898989'}} >Users</h5>
+            <h5 className='main-name'>Users</h5>
             <h2  style={{color:'#898989'}} className="main-greater-icons" >‹</h2>
           </div>
         </div>
         <div className="main-elements">
           <img className="icons" src={documents} alt="mains"></img>
           <div className="main-name-icons">
-            <h5 style={{color:'#898989'}} >Documents</h5>
+            <h5 className='main-name'>Documents</h5>
             <h2 style={{color:'#898989'}}  className="main-greater-icons" >‹</h2>
           </div>
         </div>
         <div className="main-elements">
           <img className="icons" src={applications} alt="mains"></img>
           <div className="main-name-icons">
-            <h5 style={{color:'#898989'}} >Applications</h5>
+            <h5 className='main-name'>Applications</h5>
             <h2  style={{color:'#898989'}} className="main-greater-icons" >‹</h2>
           </div>
         </div>
         <div className="main-elements">
           <img className="icons" src={pages} alt="mains"></img>
           <div className="main-name-icons">
-            <h5 style={{color:'#898989'}} >Pages</h5>
+            <h5 className='main-name'>Pages</h5>
             <h2  style={{color:'#898989'}} className="main-greater-icons" >‹</h2>
           </div>
         </div>
@@ -70,30 +70,30 @@ export default function Sidebar() {
       <h5 style={{marginBottom:'5px'}} >Secondary</h5>
         <div className="main-elements">
           <img className="icons" src={support} alt="mains"></img>
-          <h5 style={{color:'#898989'}} >Support Center</h5>
+          <h5 className='main-name'>Support Center</h5>
         </div>
         <div className="main-elements">
           <img className="icons" src={inbox} alt="mains"></img>
-          <h5 style={{color:'#898989'}} >Inbox</h5>
+          <h5 className='main-name'>Inbox</h5>
         </div>
         <div className="main-elements">
           <img className="icons" src={file} alt="mains"></img>
-          <h5 style={{color:'#898989'}} >File Manager</h5>
+          <h5  className='main-name'>File Manager</h5>
         </div>
         <div className="main-elements">
           <img className="icons" src={list} alt="mains"></img>
-          <h5 style={{color:'#898989'}} >Data List</h5>
+          <h5  className='main-name'>Data List</h5>
         </div>
       </div>
       <div className="extra-container">
       <h5 style={{marginBottom:'5px'}} > </h5>
         <div className="main-elements">
           <img className="icons" src={settings} alt="mains"></img>
-          <h5 style={{color:'#898989'}} >Settings</h5>
+          <h5 className='main-name' >Settings</h5>
         </div>
         <div className="main-elements">
           <img className="icons" src={logout} alt="mains"></img>
-          <h5 style={{color:'#898989'}} >Log Out</h5>
+          <h5  className='main-name'>Log Out</h5>
         </div>
       </div>
     </div>
